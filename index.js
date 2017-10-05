1efc2de11510e10f4d20f6a76da0d13630905c6b
Pepyn Swagemakers <ps222nq@student.lnu.se>
2017-10-05 16:42:33 +0200

    feat: retrieving answers and processing input data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const config = require('./config/Config');
 const FetchPage = require('./scraper/FetchPage');
 const MapHTMLToCheerio = require('./scraper/MapHTMLToCheerio');
 const ExtractElementObject = require('./scraper/ExtractElementObject');
+const slicer = require('./lib/stringToQuestionAlternatives');
 
 
    FetchPage(config.baseUrl + config.urlPaths[0])
@@ -15,10 +16,15 @@ const ExtractElementObject = require('./scraper/ExtractElementObject');
 
         let questionText = extractedForQuestion['0'].children[0].data;
         console.log('question: ' + questionText);
-/*
-        let answers = extracted['0'].children[0].data;
-        console.log('answers: ' + answers);
-*/
+
+        let selectorForAnswers = '.entry-content p';
+        let extractedForAnswers = ExtractElementObject(pageObject, selectorForAnswers);
+        let answersText = extractedForAnswers.text();
+        let alts = slicer(answersText);
+        for (let i = 0; i < 4; i += 1){
+            console.log(alts[i].answerAlternativeId + ": " + alts[i].answerAlternativeText);
+        }
+
         let selectorForRightAnswer = '.hidden-div b'
         let extractedForRightAnswer = ExtractElementObject(pageObject, selectorForRightAnswer);
         let rightAnswerText = extractedForRightAnswer['0'].children[0].data;
---
+++ models/AnswerAlternative.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = function(id, text) {
+    return {
+        answerAlternativeId: id,
+        answerAlternativeText: text
+    };
+}
